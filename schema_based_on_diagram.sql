ae32b41d08e67ac86d5a2d05c1f5dca2bf8b88e6
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-07-18 11:04:28 +0000

    Add foreign key & indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -48,3 +48,20 @@ CREATE TABLE medical_treatment (
     treatment_id INT,
     medical_history__id INT
 );
+
+CREATE INDEX invoice_items_invoice_id_idx ON invoice_items(invoice_id);
+CREATE INDEX invoice_items_treatment_id_idx ON invoice_items(treatment_id);
+
+CREATE INDEX invoices_medical_history_id_idx ON invoices(medical_history__id);
+
+CREATE INDEX helper_table_med_history_id_idx ON medical_treatment(treatment_id);
+CREATE INDEX helper_table_treatment_id_idx ON medical_treatment(medical_history__id);
+
+CREATE INDEX medical_histories_patient_id_idx ON medical_histories(patient_id);
+
+ALTER TABLE invoice_items ADD CONSTRAINT invoice_items_invoice_id_fkey FOREIGN KEY (invoice_id) REFERENCES invoices (id);
+ALTER TABLE invoice_items ADD CONSTRAINT invoice_items_treatment_id_fkey FOREIGN KEY (treatment_id) REFERENCES treatments (id);
+ALTER TABLE invoices ADD CONSTRAINT invoices_medical_history_id_fkey FOREIGN KEY (medical_history__id) REFERENCES medical_histories (id);
+ALTER TABLE medical_treatment ADD CONSTRAINT helper_table_med_history_id_fkey FOREIGN KEY (medical_history__id) REFERENCES medical_histories (id);
+ALTER TABLE medical_treatment ADD CONSTRAINT helper_table_treatment_id_fkey FOREIGN KEY (treatment_id) REFERENCES treatments (id);
+ALTER TABLE medical_histories ADD CONSTRAINT medical_histories_patient_id_fkey FOREIGN KEY (patient_id) REFERENCES patients (id);
